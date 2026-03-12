a7bcc4954f749b4b8ea616acdd1f8640ec593974
mdruhulamin7 <ruhulthisis@gmail.com>
2026-03-12 20:12:59 +0600

    Change Env Format

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from 'next';
 
-const backendUrl = process.env.BACKEND_URL;
+const backendUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: `${backendUrl}/api/v1/:path*`,
+        destination: `${backendUrl}/:path*`,
       },
     ];
   },
